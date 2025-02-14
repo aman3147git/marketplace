282c6955a9f79349a2fe26e29314da69b6dc4efd
aman3147git <amanc3147@gmail.com>
2025-02-14 22:55:37 +0530

    ..

--- todo/src/utils/constant.js
+++ todo/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const END_POINT="https://marketplace-xeq4.onrender.com/s1/user"
-export const END_POINT2="https://marketplace-xeq4.onrender.com/s1/listing"
+export const END_POINT="https://marketplace-xeq4.onrender.com/user"
+export const END_POINT2="https://marketplace-xeq4.onrender.com/listing"
