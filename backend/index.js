835d42b1e3d8f2028cb46402e1e67d16a87fd34f
aman3147git <amanc3147@gmail.com>
2025-03-20 16:18:29 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,12 @@ import myroute from "./routes/myroute.js";
 import mylisting from "./routes/mylisting.js";
 import cors from "cors";
 import db from "./utils/database.js";
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app=express();
+const __dirname=path.resolve();
 db();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -19,6 +21,11 @@ const corsoption={
 app.use(cors(corsoption));
 app.use("/s1/user",myroute);
 app.use("/s1/listing",mylisting);
+
+app.use(express.static(path.join(__dirname,"/todo/dist")));
+app.use("*",(_,res)=>{
+    res.sendFile(path.resolve(__dirname,"todo","dist","index.html"));
+})
 app.listen(process.env.PORT,()=>{
     console.log("server started successfully");
 });
