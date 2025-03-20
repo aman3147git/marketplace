5596d9be8d130536970d624f775476094fa131e7
aman3147git <amanc3147@gmail.com>
2025-03-20 16:41:00 +0530

    ..

--- todo/src/utils/constant.js
+++ todo/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const END_POINT="http://localhost:8080/s1/user"
-export const END_POINT2="http://localhost:8080/s1/listing"
+export const END_POINT="https://marketplace-48gl.onrender.com/s1/user"
+export const END_POINT2="https://marketplace-48gl.onrender.com/s1/listing"
